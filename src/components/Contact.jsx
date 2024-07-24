0bc3ed270c1365d4393259d2c06bd13c08ac9fa9
Liciacodes <udosenfelicia@gmail.com>
2024-07-24 20:46:17 +0100

    fixed the footer

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 
 function Contact() {
   const Separator = () => (
-    <span className="text-[#B9A390] mx-1  h-[22px] text-sm align-middle">
+    <span className="text-[#B9A390] mx-1  h-[22px] text-sm align-middle hidden md:block">
       |
     </span>
   );
@@ -18,15 +18,15 @@ function Contact() {
         <h2 className="text-xl border-l-4  border-[#F7861D] pl-2 text-[18px] font-normal mb-4 text-black leading-[22.79px]">
           Say Hello
         </h2>
-        <p className="mb-1">
+        <p className="mb-2">
           <a
             href="mailto:udosenfelicia@gmail.com"
-            className="text-black font-normal md:text-[26px] md:text-lg text-sm"
+            className="text-black font-normal md:text-[26px] text-lg "
           >
             udosenfelicia@gmail.com
           </a>
         </p>
-        <div className="flex text-black gap-x-3 mb-3">
+        <div className="flex flex-col md:flex-row space-y-3 md:space-y-0 text-black gap-x-3 mb-3">
           <a
             href="https://www.linkedin.com/in/felicia-udosen-74828923a/"
             target="_blank"
