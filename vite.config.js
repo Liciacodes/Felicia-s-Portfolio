fc6595c55ea5aeb27a169e99b4a28f7983d2a4a9
Liciacodes <udosenfelicia@gmail.com>
2026-02-27 18:54:21 +0100

    fix netlify config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import sitemap from 'vite-plugin-sitemap'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    sitemap({
-      hostname: 'https://feliciaudosen.com'
-    })
-  ],
+  plugins: [react()],
 })
